e1208d19581c2ec5a265d53a1a11b47ba270d768
tvshreyanshi <tvshreyanshi@gmail.com>
2023-10-27 17:47:08 +0530

    pickeryear

--- src/components/PickerYear.vue
+++ src/components/PickerYear.vue
@@ -17,7 +17,7 @@
       v-for="year in years"
       :key="year.timestamp"
       :class="{ 'selected': year.isSelected, 'disabled': year.isDisabled }"
-      @click.stop="selectYear(year)">{{ year.year }}-{{ year.year+1 }}</span>
+      @click.stop="selectYear(year)">{{ year.year }}</span>
   </div>
 </template>
 <script>
